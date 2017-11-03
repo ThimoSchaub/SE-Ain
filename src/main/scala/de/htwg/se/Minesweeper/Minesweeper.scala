f015d4314fd666b45f9464ea4148fb3392dfbab9
ThimoSchaub <thimoschaub@web.de>
2017-11-03 12:43:52 +0100

    Stand 03.11.2017

--- src/main/scala/de/htwg/se/Minesweeper/Minesweeper.scala
+++ src/main/scala/de/htwg/se/Minesweeper/Minesweeper.scala
@@ -1,10 +1,21 @@
 package de.htwg.se.Minesweeper
 
-import de.htwg.se.Minesweeper.model.Player
+import de.htwg.se.Minesweeper.model.{Cell, Field, Player}
 
 object Minesweeper {
   def main(args: Array[String]): Unit = {
     val student = Player("Your Name")
     println("Hello, " + student.name)
-  }
+    val feld = new Field(4,4,0)
+    for{
+      row <- 0 until 4
+      col <- 0 until 4
+    }{
+      feld.setCell(row,col,new Cell(1))
+    }
+
+
+    feld.getCell(1,1).setVisibility(true)
+    println(feld)
+    }
 }
---
+++ src/test/scala/de/htwg/se/Minesweeper/model/FieldSpec.scala
@@ -0,0 +1,5 @@
+package de.htwg.se.Minesweeper.model
+
+class FieldSpec {
+
+}
