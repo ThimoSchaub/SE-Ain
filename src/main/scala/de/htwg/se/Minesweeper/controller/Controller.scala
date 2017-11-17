3a2df3debbde37dd64a7356140caecbfb4cb8c38
ThimoSchaub <thimoschaub@web.de>
2017-11-17 13:01:03 +0100

    new methods implemented

--- src/main/scala/de/htwg/se/Minesweeper/controller/Controller.scala
+++ src/main/scala/de/htwg/se/Minesweeper/controller/Controller.scala
@@ -4,9 +4,20 @@ import de.htwg.se.Minesweeper.model.{Field, Cell}
 import de.htwg.se.Minesweeper.util.Observable
 
 class Controller(var field:Field) extends Observable{
-  def createRandomField(xSize: Int, ySize: Int):Unit = {
+  def createRandomField(xSize: Int, ySize: Int, mines: Int):Unit = {
     field = new Field(xSize, ySize, field.mines)
     notifyObservers
   }
 
+  def performAction(row: Int, col: Int, action: Int):Unit = {
+    field.performAction(row, col, action)
+    notifyObservers
+  }
+
+  def fieldToString: String = field.toString
+
+  def solve: Unit = {
+    field.allVisible
+    notifyObservers
+  }
 }
--- src/main/scala/de/htwg/se/Minesweeper/model/Cell.scala
+++ src/main/scala/de/htwg/se/Minesweeper/model/Cell.scala
@@ -1,27 +1,50 @@
 package de.htwg.se.Minesweeper.model
 
 class Cell() {
+  val MINE = 9
   var isVisible = false
   var state = 0
+  var flag = false
 
   def getState(): Int = {
     return state
   }
-
   def setState(int: Int): Unit ={
     state = int
-  }
-  def setVisibility(visible:Boolean)={
-    isVisible = visible
   }
 
   def getVisibility():Boolean={
     return isVisible
   }
+  def setVisibility(visible:Boolean)={
+    isVisible = visible
+  }
+
+  def getFlag(): Boolean = {
+    return flag
+  }
+  def setFlag(toFlag: Boolean): Unit = {
+    flag = toFlag
+  }
+  def toggleFlag(): Unit = {
+    flag = !flag
+  }
+
+  def check(): Unit = {
+    if (getState() == MINE) {
+      // LOSE!
+    }
+    else {
+      setVisibility(true)
+    }
+  }
 
   override def toString(): String = {
     if (isVisible) {
       return state.toString
+    }
+    else if (flag) {
+      return "F"
     }
     else {
       return " "
