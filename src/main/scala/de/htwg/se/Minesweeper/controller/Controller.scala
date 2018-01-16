b91f20cc5cb2e29575030f462fe8f1193aa20d6e
ThimoSchaub <thimoschaub@web.de>
2018-01-16 10:05:56 +0100

    LAGS HAVE NOW A NICE STRING REPRESANTION

--- src/main/scala/de/htwg/se/Minesweeper/controller/Controller.scala
+++ src/main/scala/de/htwg/se/Minesweeper/controller/Controller.scala
@@ -19,9 +19,9 @@ class Controller(var field:Field) extends Observable {
     notifyObservers
   }
 
-  def fieldToString: String = field.toString 
+  def fieldToString: String = field.toString
 
-  def set(row: Int, col: Int, action: Int): Unit = {
+  def set(row: Int, col: Int, action: Int): Unit = { 
     undoManager.doStep(new SetCommand(row, col, action, this))
     notifyObservers
   }
